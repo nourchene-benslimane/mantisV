80ca06f299056967735e584bd6305daa918199a1
nourchene ben slimane <nourchene.benslimane@gmail.com>
2012-08-17 00:04:21 +0100

    version 1.1: user table updated

---
+++ mantis/config_inc.php
@@ -0,0 +1,21 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'mantisdb';
+	$g_db_username = 'mantis';
+	$g_db_password = 'mantis';
+	
+	$g_default_language='french'; 
+	$g_short_date_format='d-m-Y'; 
+	$g_normal_date_format='d-m-Y H:i'; 
+	$g_complete_date_format='d-m-Y H:i'; 
+	
+	#$g_logo_image			= 'images/__mantis_logo.jpg';
+	$g_logo_image			= 'images/red-banner.jpg';
+	
+	#enabling news
+	$g_news_enabled = ON;
+	
+	#allowing a bug to have no category
+	$g_allow_no_category = OFF;
+?>
